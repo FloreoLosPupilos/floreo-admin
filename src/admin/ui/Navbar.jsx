142271d6c63b955faa77e2edfbe490f770b01ce7
kwalsh15 <kwalsh@pernixlabs.com>
2022-09-17 20:41:39 -0600

    Remove services from navbar

--- src/admin/ui/Navbar.jsx
+++ src/admin/ui/Navbar.jsx
@@ -15,7 +15,7 @@ import { useDispatch } from 'react-redux';
 import { startLogout } from '../../auth';
 import { Avatar, Tooltip } from '@mui/material';
 
-const pages = ['about', 'carrousel', 'blog', 'servicios', 'categoria', 'integrantes'];
+const pages = ['about', 'carrousel', 'blog', 'categoria', 'integrantes'];
 
 export const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
