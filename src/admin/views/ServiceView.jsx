d834f7904aae31d2b1fe1f937dd76ab44c19fafe
kwalsh15 <kwalsh@pernixlabs.com>
2022-09-17 20:42:40 -0600

    create modal for 'add service' action

--- src/admin/views/ServiceView.jsx
+++ src/admin/views/ServiceView.jsx
@@ -1,11 +1,13 @@
 import {Tabla} from "../components/tabla/tabla";
 import {useLocation} from 'react-router-dom';
+import { AddServiceModalView } from "./AddServiceModalView";
 
 export const ServiceView = () => {
   const location = useLocation();
   return (
     
     <>
+    <AddServiceModalView />
     <Tabla collection={location.state.nombre} subCollection={true}/>
     </>
   );
