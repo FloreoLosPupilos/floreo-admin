b4333a0a4d586bec5c2d1815ae25d07419c0a1f7
alemiranda1 <38299991+alemiranda1@users.noreply.github.com>
2022-09-28 21:33:40 -0600

    Confirmation for member deletion / admin table
    Added the confirmation modal for the member deletion function.

--- src/hooks/useColumnsMembers.jsx
+++ src/hooks/useColumnsMembers.jsx
@@ -1,20 +1,45 @@
 import { useMemo } from "react";
+
+//Firebase
 import { doc, deleteDoc, updateDoc, collection} from "firebase/firestore/lite";
 import { FirebaseDB } from "../firebase/config";
+import { isAsyncThunkAction } from "@reduxjs/toolkit";
+
+//Modal de editar
+import { EditMemberModalView } from "../admin/views/EditMemberModalView";
+import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
+
+//Dialog
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+import { IconButton } from "@mui/material";
 
 //Metodo para editar un miembro.
 function editMember(id) {
   console.log("Editar", id);
-  
 };
 
 //Metodo para eliminar un miembro.
 const deleteMember = async (id) => {
-  console.log(id)
-  const memberDoc = doc(FirebaseDB, "Integrantes", id.id);
-  await deleteDoc(memberDoc);
-}
 
+  const remove = async (id) => {
+    const memberDoc = doc(FirebaseDB, "Integrantes", id.id);
+    await deleteDoc(memberDoc);
+  }
+  
+  Swal.fire({
+    title: '¿Está seguro de eliminar el integrante: ' + id.nombre + '?',
+    showCancelButton: true,
+    confirmButtonText: 'Eliminar',
+    denyButtonText: `Cancelar`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      remove(id);
+      Swal.fire('Integrante ' + id.nombre + ' eliminado correctamente', '', 'success');
+    }
+  })
+}
 
 export const useColumnsMembers = () => {
   const columns = useMemo(
@@ -43,12 +68,8 @@ export const useColumnsMembers = () => {
           const rowIdx = props.row.id;
           return (
             <div>
-              <span onClick={() => editMember(props.row.original)}>
-                <button style={{}}>Editar</button>
-              </span>
-              <span onClick={() => deleteMember(props.row.original)}>
-                <button style={{}}>Eliminar</button>
-              </span>
+              
+              <IconButton onClick={() => deleteMember(props.row.original)} style={{}}><DeleteIcon /></IconButton>
             </div>
           );
         },
