ecc90e501eb97589189c1e578fbb895665657276
kwalsh15 <kwalsh@pernixlabs.com>
2022-09-30 16:04:37 -0600

    Fix github 404 error when reloading website

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { AdminApp } from './AdminApp';
@@ -10,9 +10,9 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter>
+      <HashRouter>
         <AdminApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
