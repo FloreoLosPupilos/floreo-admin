f8baaf9d723ff2ea9b74e09cc44c21963a01f4fa
luis-Tec <acl0076@gmail.com>
2022-10-02 21:58:21 -0600

    Update ServiceView.jsx

--- src/admin/views/ServiceView.jsx
+++ src/admin/views/ServiceView.jsx
@@ -1,16 +1,16 @@
-import {Tabla} from "../components/tabla/tabla";
-import {useLocation} from 'react-router-dom';
+import { Tabla } from "../components/tabla/tabla";
+import { useLocation } from 'react-router-dom';
 import { AddServiceModalView } from "./AddServiceModalView";
 import { Title } from "./Title";
 
 export const ServiceView = () => {
   const location = useLocation();
   return (
-    
+
     <>
-    <Title title='Servicios' />
-    <AddServiceModalView />
-    <Tabla collection={location.state.id} subCollection={true}/>
+      <Title title={"Servicios - " + location.state.nombre} />
+      <AddServiceModalView />
+      <Tabla collection={location.state.id} subCollection="true" />
     </>
   );
 }
