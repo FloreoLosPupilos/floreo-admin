d89ad6b9324bea36fc8a7bf4e09a99e784c05cb1
luis-Tec <acl0076@gmail.com>
2022-10-02 21:58:13 -0600

    Update setCollectionDataCustomId.js

--- src/helpers/setCollectionDataCustomId.js
+++ src/helpers/setCollectionDataCustomId.js
@@ -1,7 +1,21 @@
 import { FirebaseDB } from '../firebase/config';
-import { doc, collection, setDoc} from 'firebase/firestore/lite';
+import { doc, collection, setDoc } from 'firebase/firestore/lite';
+import { startLoadingCategories, startLoadingMembers, startLoadingServices } from '../store/collections/thunks';
 
-export const setCollectionDataCustomId = async (collectionName, data, id) => {
-  const newDoc = doc( collection( FirebaseDB, collectionName ), id );
+export const setCollectionDataCustomId = async (collectionName, data, id, dis) => {
+  const newDoc = doc(collection(FirebaseDB, collectionName), id);
   await setDoc(newDoc, data);
+  dis(startLoadingCategories());
+}
+
+export const setServicesCustomId = async (collectionName, data, id, dis) => {
+  const newDoc = doc(collection(FirebaseDB, collectionName), id);
+  await setDoc(newDoc, data);
+  dis(startLoadingServices());
+}
+
+export const setMembersCustomId = async (collectionName, data, id, dis) => {
+  const newDoc = doc(collection(FirebaseDB, collectionName), id);
+  await setDoc(newDoc, data);
+  dis(startLoadingMembers());
 }
