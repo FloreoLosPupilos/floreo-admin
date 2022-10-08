7ced0bd4d6057ced61dcbb66aaafd1f174af89ab
luis-Tec <acl0076@gmail.com>
2022-10-08 09:55:37 -0600

    Update setCollectionDataCustomId.js

--- src/helpers/setCollectionDataCustomId.js
+++ src/helpers/setCollectionDataCustomId.js
@@ -1,6 +1,6 @@
 import { FirebaseDB } from '../firebase/config';
 import { doc, collection, setDoc } from 'firebase/firestore/lite';
-import { startLoadingCategories, startLoadingMembers, startLoadingServices } from '../store/collections/thunks';
+import { addCustomServices, startLoadingCategories, startLoadingMembers, startLoadingServices } from '../store/collections/thunks';
 
 export const setCollectionDataCustomId = async (collectionName, data, id, dis) => {
   const newDoc = doc(collection(FirebaseDB, collectionName), id);
@@ -18,4 +18,18 @@ export const setMembersCustomId = async (collectionName, data, id, dis) => {
   const newDoc = doc(collection(FirebaseDB, collectionName), id);
   await setDoc(newDoc, data);
   dis(startLoadingMembers());
+}
+
+export const setMembersData = async (collectionName, data, dis) => {
+  const newDoc = doc(collection(FirebaseDB, collectionName));
+  await setDoc(newDoc, data);
+  dis(startLoadingMembers());
+}
+
+export const setServicesData = async (collectionName, data, dis) => {
+  const newDoc = doc(collection(FirebaseDB, collectionName));
+  await setDoc(newDoc, data);
+  const addService = { nombre: data.nombre, informacion: data.informacion, precio: data.precio, categoria: data.categoria, id: newDoc.id };
+  dis(startLoadingServices());
+  dis(addCustomServices(addService));
 }
