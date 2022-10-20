f730347b6143475191a065e49b1ea84f0a76304e
luis-Tec <acl0076@gmail.com>
2022-10-20 01:10:27 -0600

    Create modal to view orders

---
+++ src/admin/layout/ModalOrderLayout.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { Box, Modal, Typography, IconButton } from '@mui/material';
+import { HighlightOff } from '@mui/icons-material';
+import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
+export const ModalOrderLayout = ({ children, modalTitle }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const closeModal = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '70%',
+    height: '90vh',
+    bgcolor: 'background.paper',
+    borderRadius: '10px',
+    boxShadow: 24,
+    p: 4,
+
+    //Scroll
+    overflow: "auto",
+    scrollbarWidth: 'thin',
+    scrollbarColor: '#B7B7B7 transparent',
+    '&::-webkit-scrollbar': {
+      width: 10,
+      height: 6,
+      backgroundColor: 'transparent',
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: 'transparent',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      borderRadius: 6,
+      backgroundColor: '#B7B7B7',
+      minHeight: 24,
+      minWidth: 24,
+    },
+    '&::-webkit-scrollbar-thumb:focus': {
+      backgroundColor: '#adadad',
+    },
+    '&::-webkit-scrollbar-thumb:active': {
+      backgroundColor: '#adadad',
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: '#adadad',
+    },
+    '&::-webkit-scrollbar-corner': {
+      backgroundColor: 'transparent',
+    },
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason && reason == "backdropClick")
+      return;
+    closeModal();
+  }
+
+  window.addEventListener('closeModal', () => {
+    closeModal();
+  });
+
+  return (
+    <>
+      <IconButton onClick={handleOpen}><VisibilityRoundedIcon style={{ fill: "#0000E0" }} /></IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+
+      >
+        <Box scroll='paper' sx={style}>
+          <Typography sx={{ fontSize: '14' }} display="inline" id="modal-modal-title" variant="h6" component="h2">
+            {modalTitle}
+          </Typography>
+          <div style={{ position: 'sticky', top: 0, float: 'right' }}>
+            <IconButton onClick={handleClose}>
+              <HighlightOff sx={{ fontSize: 35 }} />
+            </IconButton>
+          </div>
+          <div>
+            {children}
+          </div>
+        </Box>
+      </Modal>
+    </>
+  );
+}
---
+++ src/admin/views/OrderModalView.jsx
@@ -0,0 +1,167 @@
+import { ModalOrderLayout } from "../layout/ModalOrderLayout";
+import { Button, Card, CardContent, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import Swal from 'sweetalert2';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+
+export const OrderModalView = (props) => {
+  const position = props.data.ubicacion;
+
+  const evt = new CustomEvent("closeModal");
+
+  function servicesList() {
+    return (
+      <ol>
+        {props.data.servicios.map(service => (
+          <li key={service}>{service}</li>
+        ))}
+      </ol>
+    );
+  }
+
+  const accpetOrder = async () => {
+    Swal.fire({
+      title: '¿Está seguro de aceptar este pedido?',
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Este pedido se ha aceptado correctmente', '', 'success');
+        window.dispatchEvent(evt);
+      }
+    })
+  }
+
+  const rejectOrder = async () => {
+    Swal.fire({
+      title: '¿Está seguro de rechazar este pedido?',
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Este pedido se ha rechazado correctmente', '', 'success');
+        window.dispatchEvent(evt);
+
+      }
+    })
+  }
+
+  return (
+    <>
+      <ModalOrderLayout modalTitle='Pedido'>
+        <div>
+          <Card style={{ width: '100%' }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14, fontWeight: '999' }} color="black" gutterBottom>
+                Cliente
+              </Typography>
+              <Typography variant="body2">
+                Nombre: {props.data.nombre}
+                <br />
+                Teléfono: {props.data.telefono}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <br />
+        <div>
+          <Typography style={{ fontSize: '14', width: '100%', display: 'inline', fontWeight: '999' }} component="h2" color="text.primary" >
+            Ubicación
+          </Typography>
+          <MapContainer
+            center={position}
+            zoom={14}
+            style={{ height: '35vh', width: '100%' }}
+          >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup >
+                Ubiccaión
+              </Popup>
+            </Marker>
+          </MapContainer>
+
+          <Card style={{ width: '100%' }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14, fontWeight: '999' }} color="black" gutterBottom>
+                Dirección
+              </Typography>
+
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {props.data.distrito}, {props.data.canton}, {props.data.provincia}.
+              </Typography>
+
+              <Typography variant="body2">
+                {props.data.direccion}
+              </Typography>
+              <br />
+              <Typography sx={{ fontWeight: '999', float: 'right' }} display="inline">
+                Fecha: {props.data.fecha}, Hora: {props.data.hora}
+              </Typography>
+            </CardContent>
+          </Card>
+          <br />
+          <Card style={{ width: '100%' }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14, fontWeight: '999' }} color="black" gutterBottom>
+                Servicios
+              </Typography>
+
+
+              {
+                servicesList()
+              }
+
+
+            </CardContent>
+          </Card>
+        </div>
+        <hr style={{ height: '4.5px', backgroundColor: 'black' }} />
+        <div>
+          <Typography display="inline" sx={{ fontSize: 28, fontWeight: '999' }} color="text.primary" gutterBottom>
+            Total:
+          </Typography>
+          <Typography sx={{ fontSize: 28, float: 'right', fontWeight: '600' }} variant="body2">
+            ₡{props.data.total}
+          </Typography>
+        </div>
+        <div>
+          <Button onClick={() => accpetOrder()}
+            color='primary'
+            sx={{
+              fontSize: 14, float: 'right', marginLeft: '40px',
+              ':hover': {
+                bgcolor: '#90EE90', // theme.palette.primary.main
+                color: 'primary',
+              },
+            }}
+          >
+            <CheckCircleOutlineIcon sx={{ fontSize: 35, mr: 1, fill: 'green' }} />
+            Aceptar
+          </Button>
+          <Button onClick={() => rejectOrder()}
+            color='primary'
+            sx={{
+              fontSize: 14, float: 'right', borderRadius: '10px', border: 'black', ':hover': {
+                bgcolor: '#FFCCCB', // theme.palette.primary.main
+                color: 'primary',
+              }
+            }}
+          >
+            <HighlightOffIcon sx={{ fontSize: 35, mr: 1, fill: 'red' }} />
+            Rechazar
+          </Button>
+        </div>
+      </ModalOrderLayout>
+    </>
+  );
+}
