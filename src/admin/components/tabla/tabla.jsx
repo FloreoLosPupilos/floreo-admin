51ab15222bdc6d2a83b333b4ed1c0c9a09c3392c
luis-Tec <acl0076@gmail.com>
2022-10-23 14:28:04 -0600

    Fixing filter

--- src/admin/components/tabla/tabla.jsx
+++ src/admin/components/tabla/tabla.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 //Icons
 import FirstPageIcon from '@mui/icons-material/FirstPage';
 import LastPageIcon from '@mui/icons-material/LastPage';
-import { IconButton } from '@mui/material';
+import { FormControl, IconButton, InputLabel, MenuItem, Select, Typography } from '@mui/material';
 
 //Paginacion & Filtro
 import { useTable, usePagination, useGlobalFilter, useAsyncDebounce } from "react-table";
@@ -28,8 +28,9 @@ function imgDataLoader(cell) {
 }
 
 //Filtro de busqueda
-function Filter({ globalFilter, setGlobalFilter }) {
+function Filter({ globalFilter, setGlobalFilter, props }) {
   const [value, setValue] = useState(globalFilter);
+  const [state, setState] = React.useState('');
 
   const onFilterChange = useAsyncDebounce(
     (value) => setGlobalFilter(value || undefined),
@@ -41,17 +42,52 @@ function Filter({ globalFilter, setGlobalFilter }) {
     onFilterChange(e.target.value);
   };
 
+  const handleChange = (event) => {
+    onFilterChange(event.target.value);
+    setState(event.target.value);
+  };
+
   return (
-    <span style={{ backgroundColor: "white" }}>
-      <input
-        size={40}
-        value={value || ""}
-        onChange={handleInputChange}
-        placeholder="Buscar..."
-      />
-    </span>
+    <>
+      <span>
+        <input
+          size={40}
+          value={value || ""}
+          onChange={handleInputChange}
+          placeholder="Buscar..."
+        />
+        {
+          (() => {
+            if (props.collection == 'Pedidos') {
+              return (
+                <FormControl sx={{ m: -0.5, minWidth: 80, marginInline: '15px' }} size="small">
+                  <InputLabel id="demo-select-small">Filtro</InputLabel>
+                  <Select
+                    value={state}
+                    label="Filtros"
+                    onChange={handleChange}
+                    sx={{
+                      height: '35px',
+                      backgroundColor: 'white'
+                    }}
+
+                  >
+                    <MenuItem value=" ">Ninguno</MenuItem>
+                    <MenuItem value="Pendiente">Pendientes</MenuItem>
+                    <MenuItem value="Aceptado">Aceptados</MenuItem>
+                    <MenuItem value="Rechazado">Rechazados</MenuItem>
+                  </Select>
+                </FormControl>
+              )
+            }
+          })()
+        }
+      </span>
+    </>
+
   );
 }
+
 
 //Asignar los valores que va a tener la columna dependiedo de la colleccion que se pida.
 function getColumsData(collection, subcollection) {
@@ -65,30 +101,33 @@ function getColumsData(collection, subcollection) {
     return (useColumnsSubcribers());
   } else if (collection == "Pedidos") {
     return (useColumnsOrders());
-  } else {
-    return ("Error");
   }
 }
 
 //Componente Tabla
 export const Tabla = (props) => {
   let collection = [];
+  let title = "";
 
   if (props.subCollection) {
     collection = useSelector(state => state.collections.customServices);
+    title = "Servicios";
   } else if (props.collection == "Categorias") {
     collection = useSelector(state => state.collections.categories);
+    title = "Categorias";
   } else if (props.collection == "Integrantes") {
     collection = useSelector(state => state.collections.members);
+    title = "Integrantes";
   } else if (props.collection == "Suscriptores") {
     collection = useSelector(state => state.collections.subscribers);
+    title = "Suscriptores";
   } else if (props.collection == "Pedidos") {
     collection = useSelector(state => state.collections.orders);
+    title = "Pedidos";
   }
 
   //Columnas
   const columns = getColumsData(props.collection, props.subCollection);
-
 
   //Datos de las filas
   const data = useMemo(
@@ -113,7 +152,6 @@ export const Tabla = (props) => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
     preGlobalFilteredRows,
     setGlobalFilter,
@@ -130,107 +168,131 @@ export const Tabla = (props) => {
   } = table;
 
   return (
-    <div className="container" style={{ paddingTop: 0 }}>
-      <div className='search' style={{ margin: "0px" }}>
+    <>
+      <div className='search' style={{ zIndex: 900, backgroundcolor: 'white', height: '30px', padding: '2rem 5rem' }}>
         <Filter
           preGlobalFilteredRows={preGlobalFilteredRows}
           globalFilter={globalFilter}
           setGlobalFilter={setGlobalFilter}
+          props={props}
         />
       </div>
-      <table {...getTableProps()}>
-        <thead>
-          {
-            // Loop over the header rows
-            headerGroups.map((headerGroup) => (
-              // Apply the header row props
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  // Loop over the headers in each row
-                  headerGroup.headers.map((column) => (
-                    // Apply the header cell props
-                    <th className='colums'{...column.getHeaderProps()}>
-                      {
-                        // Render the header
-                        column.render("Header")
-                      }
-                    </th>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </thead>
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {
-            // Loop over the table rows
-            page.map((row) => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map((cell) => {
-                      // Apply the cell props
-                      return (
-                        <td data={cell.column.Header} {...cell.getCellProps()}>
-
-                          <div>
-                            {
-
-                              // Render the cell contents
-                              cell.render("Cell")
-                            }
-                          </div>
-                          {imgDataLoader(cell)}
-
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
-      <div className="pagination">
-        <span>
-          Página&nbsp;
-          <strong>
-            {pageIndex + 1} de {pageOptions.length}
-          </strong>{" "}
-        </span>
-        <div>
-          <IconButton onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-            <FirstPageIcon />
-          </IconButton>{" "}
-          <IconButton onClick={() => previousPage()} disabled={!canPreviousPage}>
-            Atrás
-          </IconButton>{" "}
-          <IconButton onClick={() => nextPage()} disabled={!canNextPage}>
-            Siguiente
-          </IconButton>{" "}
-          <IconButton onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-            <LastPageIcon />
-          </IconButton>{" "}
-        </div>
-        <select
-          value={pageSize}
-          onChange={(e) => setPageSize(Number(e.target.value))}
-        >
-          {[5, 10, 15].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              {pageSize !== 15 ? `Mostrar ${pageSize}` : `Mostrar todo`}
-            </option>
-
-          ))}
-        </select>
+      <div>
 
       </div>
-    </div>
+      <div className="container" style={{ paddingTop: 0 }}>
+        <table {...getTableProps()}>
+          <thead>
+            {
+              // Loop over the header rows
+              headerGroups.map((headerGroup) => (
+                // Apply the header row props
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {
+                    // Loop over the headers in each row
+                    headerGroup.headers.map((column) => (
+                      // Apply the header cell props
+                      <th className='columns'{...column.getHeaderProps()}>
+                        {
+                          // Render the header
+                          column.render("Header")
+                        }
+                      </th>
+                    ))
+                  }
+                </tr>
+              ))
+            }
+          </thead>
+          {/* Apply the table body props */}
+          <tbody {...getTableBodyProps()}>
+            {
+              // Loop over the table rows
+              page.map((row) => {
+                // Prepare the row for display
+                prepareRow(row);
+                return (
+                  // Apply the row props
+                  <tr {...row.getRowProps()}>
+                    {
+                      // Loop over the rows cells
+                      row.cells.map((cell) => {
+                        // Apply the cell props
+                        return (
+                          <td data={cell.column.Header} {...cell.getCellProps()}>
+
+                            <div>
+                              {
+
+                                // Render the cell contents
+                                cell.render("Cell")
+                              }
+                            </div>
+                            {imgDataLoader(cell)}
+
+                          </td>
+                        );
+                      })
+                    }
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+        {
+          (() => {
+            if (collection.length == 0) {
+              console.log(props)
+              return (
+                <div style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <Typography sx={{ fontSize: '3.5vh' }} display="inline" variant="h6" component="h1" color="text.secondary">
+                    No hay {title}
+                  </Typography>
+                </div>
+              )
+            }
+          })()
+        }
+        <div className="pagination">
+          <span>
+            Página&nbsp;
+            <strong>
+              {pageIndex + 1} de {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <div>
+            <IconButton id='paginationbutton' onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+              <FirstPageIcon />
+            </IconButton>
+            <IconButton id='paginationbutton' sx={{ maxWidth: '100%' }} onClick={() => previousPage()} disabled={!canPreviousPage}>
+              Atrás
+            </IconButton>
+            <IconButton id='paginationbutton' sx={{ maxWidth: '100%' }} onClick={() => nextPage()} disabled={!canNextPage}>
+              Siguiente
+            </IconButton>
+            <IconButton id='paginationbutton' onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+              <LastPageIcon />
+            </IconButton>
+          </div>
+          <select
+            value={pageSize}
+            onChange={(e) => setPageSize(Number(e.target.value))}
+          >
+            {[5, 10, 15].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize !== 15 ? `Mostrar ${pageSize}` : `Mostrar todo`}
+              </option>
+            ))}
+          </select>
+
+        </div>
+      </div>
+    </>
+
   );
 }
